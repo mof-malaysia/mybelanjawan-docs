fdfa9dd1468101de54c0f23f1438f4d816c36b68
Justin Koay <35253747+JustinTzeJi@users.noreply.github.com>
2025-10-01 18:24:39 +0800

    fix: baseOptions Documentation Url
    Updates the url of `Documentation` from `/docs` to `/docs/rumusan`

--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -14,7 +14,7 @@ export const baseOptions: BaseLayoutProps = {
   links: [
     {
       text: 'Documentation',
-      url: '/docs',
+      url: '/docs/rumusan',
       active: 'nested-url',
     },
   ],
